9aa821811da02489d85dd7da20469ade406731bc
SempervirensSoftware <peter.terrill@gmail.com>
2012-06-07 14:05:35 -0700

    initial commit. default rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedbackly::Application.config.secret_token = 'bfc7841cd89816587b721c059cf7223520939948d7c993ef2d2d2dd4635d9f4443d33a11cd879b59bd0cd1ea9d631b49648572ed220656606c890f30bcbaae7e'
