44046d3476bc86811ae210a8b5d630dbcee92689
SempervirensSoftware <peter.terrill@gmail.com>
2012-06-07 17:32:21 -0700

    hooked the form into the database

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Feedbackly::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
-  root :to => "feedback#new"
-  match "feedback" => "feedback#new"
+  root :to => "feedbacks#new"
+  resources :feedbacks
 
 end
