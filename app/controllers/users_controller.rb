d89d8f7e089f7ae2360f76041892a96ccd71259a
SempervirensSoftware <peter.terrill@gmail.com>
2012-06-09 12:48:09 -0700

    added log in/out buttons to the nav.added a quick table to the user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
 
     if @user.save
       pending_user.destroy
+      session[:user_id] = @user.id
       redirect_to @user
     else
       redirect_to pending_user
@@ -22,18 +23,26 @@ class UsersController < ApplicationController
   end  
   
   def show
+    # make sure they are logged in
     session_id = session[:user_id]
     if (!session_id)
       redirect_to log_in_url
       return
     end
 
+    # make sure they are accessing their own page
     id = params[:id]
     if (session_id.to_i != id.to_i )
-      redirect_to root_url
+      actual_user = User.find(session_id)
+      if actual_user
+        redirect_to actual_user
+      else
+        redirect_to log_out_url
+      end
       return
     end
     
+    # they are cool. now build the page
     @user = User.find(id)
     @feedbacks = Feedback.find_all_by_email(@user.email)
   end
