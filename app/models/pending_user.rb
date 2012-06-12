297219fe05b447e14b9320e907ea0e4996bb6136
Sid Gidwani <sgidwani@gmail.com>
2012-06-12 13:29:28 -0700

    send different emails based on whether user has signed up, received prior feedback or is brand new

--- app/models/pending_user.rb
+++ app/models/pending_user.rb
@@ -14,24 +14,29 @@ class PendingUser < ActiveRecord::Base
     pending_user = PendingUser.new
     pending_user.email = email
     pending_user.secret = secret
+    link = "http://www.nowtellem.com/pending_users/#{pending_user.secret}"
     
     if (pending_user.save)    
-      self.notify(email,feedback)
+      self.notify_new_feedback(email,feedback,link)
     end
   end
   
-  def self.notify_pending_user(email,feedback)   
-    self.notify(email,feedback)
+  def self.notify_pending_user(email,feedback,secret) 
+    mysecret = secret
+    link = "http://www.nowtellem.com/pending_users/#{mysecret}"
+    self.notify_new_feedback(email,feedback,link)
   end
   
-  def self.notify_user(email,feedback)
-      self.notify(email,feedback)
+  def self.notify_user(email,feedback,user_id)
+    myUserId = user_id
+    link = "http://www.nowtellem.com/users/#{myUserId}"
+    self.notify_new_feedback(email,feedback,link)
   end
   
-  def self.notify_new_feedback(email,feedback)
+  def self.notify_new_feedback(email,feedback,link)
     subject = "You have new feedback"
-    body = "\"#{feedback}\"\n\nDisagree? Visit NowTellEm to rate their feedback.\nhttp://www.nowtellem.com/pending_users/#{pending_user.secret}"
-    htmlBody = "<html>\"#{feedback}\"<br /><br />Disagree? Visit NowTellEm to rate their feedback.<br />http://www.nowtellem.com/pending_users/#{pending_user.secret}</html>"
+    body = "\"#{feedback}\"\n\nDisagree? Visit NowTellEm to rate their feedback.\n#{link}"
+    htmlBody = "<html>\"#{feedback}\"<br /><br />Disagree? Visit NowTellEm to rate their feedback.<br />#{link}</html>"
     UserMailer.send_email(email,subject,body,htmlBody)
   end
   
